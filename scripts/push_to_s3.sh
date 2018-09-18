080a5bd9ec5de964fb08c9be1bc20c329b57c64a
Eric Williams <ewilliams@ukcloud.com>
2018-09-18 10:36:03 +0100

    Paths

--- scripts/push_to_s3.sh
+++ scripts/push_to_s3.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # create the configuration file
-
-sed "s|ACCESS_KEY|${AWS_ACCESS_KEY}|g" s3cfg.tmpl | \
+CMDDIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+sed "s|ACCESS_KEY|${AWS_ACCESS_KEY}|g" ${CMDDIR}/s3cfg.tmpl | \
   sed "s|SECRET_KEY|${AWS_SECRET_KEY}|g" | \
   sed "s|HOST_BASE|${HOST_BASE}|g" |
   sed "s|BUCKET_NAME|${BUCKET_NAME}|g" \
-  > s3cfg
-s3cmd -c s3cfg  mb s3://"${BUCKET_NAME}" 
-s3cmd -c s3cfg sync $1 s3://${BUCKET_NAME}
+  > ${CMDDIR}/s3cfg
+${APP_ROOT}/ecscli/bin/s3cmd -c ${CMDDIR}/s3cfg  mb s3://"${BUCKET_NAME}" 
+${APP_ROOT}/ecscli/bin/s3cmd -c ${CMDDIR}/s3cfg sync $1 s3://${BUCKET_NAME}
