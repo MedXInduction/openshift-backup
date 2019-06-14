eccab869e8962a4d10aa6561b192e15c30dfffae
Terry Wilson <terry.w@inductionhealthcare.com>
2019-06-14 13:16:47 +0100

    Revert "Added `-v` switch for verbose logging."
    This reverts commit 26c18db7e76293e31513003383a829be51970649.

--- scripts/push_to_s3.sh
+++ scripts/push_to_s3.sh
@@ -7,5 +7,5 @@ sed "s|ACCESS_KEY|${AWS_ACCESS_KEY}|g" ${CMDDIR}/s3cfg.tmpl | \
   sed "s|HOST_BASE|${HOST_BASE}|g" |
   sed "s|BUCKET_NAME|${BUCKET_NAME}|g" \
   > ${CMDDIR}/s3cfg
-${APP_ROOT}/ecscli/bin/s3cmd -v -c ${CMDDIR}/s3cfg  mb s3://"${BUCKET_NAME}"
-${APP_ROOT}/ecscli/bin/s3cmd -v -c ${CMDDIR}/s3cfg sync $1 s3://${BUCKET_NAME}
+${APP_ROOT}/ecscli/bin/s3cmd -c ${CMDDIR}/s3cfg  mb s3://"${BUCKET_NAME}" 
+${APP_ROOT}/ecscli/bin/s3cmd -c ${CMDDIR}/s3cfg sync $1 s3://${BUCKET_NAME}
